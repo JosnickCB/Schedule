ebe297d5f1f74dc8618085e3543f9d6f6e47c812
JosnickCB <42551058+JosnickCB@users.noreply.github.com>
2020-11-02 01:34:11 -0500

    alpha2

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
-<!DOCTYPE html>
-
-<html>
+<!doctype html>
+<html lang=''>
 <head>
-    <meta charset="utf-8">
-
-    <title>The HTML5 Herald</title>
-    <!--meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
-    <link rel="stylesheet" href="css/styles.css?v=1.0"-->
-
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>CSS MenuMaker</title>
 </head>
 <body>
-    <!--script src="js/scripts.js"></script-->
+
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='#'>Home</a></li>
+   <li><a href='#'>Products</a></li>
+   <li><a href='#'>Company</a></li>
+   <li><a href='#'>Contact</a></li>
+</ul>
+</div>
+
 </body>
-</html>
+<html>
