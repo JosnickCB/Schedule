246c680d575b8ee083441d5da48c4c2e280f47ec
JosnickCB <42551058+JosnickCB@users.noreply.github.com>
2020-11-02 01:04:07 -0500

    alfa1

--- main.php
+++ main.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
 
     <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <!--meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
+    <link rel="stylesheet" href="css/styles.css?v=1.0"-->
 
 </head>
 <body>
