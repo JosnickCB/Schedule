28eb8c7ba0ec555095470ce15805fb2847340740
Josnick <josnick@josnick.josnick>
2020-11-08 23:23:40 -0500

    alpha82

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "gpqwpiiktacwxs"); // Usuario
+define('DB_PASSWORD', "538976debbd8d639330ebbe6ab93d11c4d7dbab4478b4d3461d9cb13a79c7420"); // Password
+define('DB_DATABASE', "d9als99827c31n"); // Nombre de la base de datos
+define('DB_SERVER', "ec2-107-22-241-205.compute-1.amazonaws.com"); // host server
+?>
