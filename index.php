8b137c53f7aede164a457b0bac20cc1c59734535
Josnick <josnick@josnick.josnick>
2020-11-08 23:01:28 -0500

    alpha8

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
    <!-- <link rel="stylesheet" href="styles.css"> -->
    <link rel="stylesheet" href="style/tablas.css">
 
-   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="js/ajax_first.js"></script>
    <!-- <script src="script.js"></script> -->
    <title>Inicio</title>
