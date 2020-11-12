93135481e4c551ab325f97d1cf8eb43b1d10229d
Edwar Vargas Herhuay <evargas@unsa.edu.pe>
2020-11-11 23:37:17 -0500

    alpha45

--- profesor.php
+++ profesor.php
@@ -6,6 +6,7 @@
     <title>Profesor</title>
 </head>
 <body>
+    <p>hola</p>
     <table style="width:100%" border="1">
     <?php
         require 'SQLGlobal.php';
@@ -18,12 +19,22 @@
         $con=pg_connect($cadena) or die ("Error conexion.".pg_last_error());
 
         $result = pg_query("SELECT u.nombre, r.usu_dni,c.nombre,r.curso_id from relacion_usuario_curso r
-        inner join usuario u on u.dni=dni_persona and r.usu_dni=dni_persona
+        inner join usuario u on u.dni=161616 and r.usu_dni=161616
             inner join curso c on c.id_curso=r.curso_id");
         $result2= pg_query("SELECT u.nombre, r.usu_dni,c.nombre,r.curso_id from relacion_usuario_curso r
         inner join usuario u on u.dni=161616 and r.usu_dni=161616
             inner join curso c on c.id_curso=r.curso_id and c.hora_entrada=hora_entrada and c.hora_salida=hora_salida");
         
+        while($row = pg_fetch_row($result,null,PGSQL_ASSOC)) {
+            echo "<li><strong>"
+                    ." id curso : ".$row['nombre']
+                    ." nombre : ".$row['dni']
+                    ." ngrupo: ".$row['nombre']
+                    ." hora entrada : ".$row['id_curso']
+                    ." hora salidad : ".$row['hora_entrda']."</strong></li>";
+                    echo "<br />\n";
+        }
+
         while($row = pg_fetch_row($result2,null,PGSQL_ASSOC)) {
             echo "<li><strong>"
                     ." id curso : ".$row['nombre']
@@ -34,7 +45,6 @@
                     ." hora salida : ".$row['hora_salida']."</strong></li>";
                     echo "<br />\n";
         }
-
     ?>
     </table>
 </body>
