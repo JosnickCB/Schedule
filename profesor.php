1f1cf69ae04ed7d41d295f1f6048e2370ea1a387
Edwar Vargas Herhuay <evargas@unsa.edu.pe>
2020-11-11 23:38:55 -0500

    alpha45

--- profesor.php
+++ profesor.php
@@ -18,9 +18,7 @@
         password='538976debbd8d639330ebbe6ab93d11c4d7dbab4478b4d3461d9cb13a79c7420'";
         $con=pg_connect($cadena) or die ("Error conexion.".pg_last_error());
 
-        $result = pg_query("SELECT u.nombre, r.usu_dni,c.nombre,r.curso_id from relacion_usuario_curso r
-        inner join usuario u on u.dni=161616 and r.usu_dni=161616
-            inner join curso c on c.id_curso=r.curso_id");
+        $result = pg_query("select get_data(161616)");
         $result2= pg_query("SELECT u.nombre, r.usu_dni,c.nombre,r.curso_id from relacion_usuario_curso r
         inner join usuario u on u.dni=161616 and r.usu_dni=161616
             inner join curso c on c.id_curso=r.curso_id and c.hora_entrada=hora_entrada and c.hora_salida=hora_salida");
